f415540d3978b80048d222b81e6b1836ee649689
yoshiki0425 <judging5@gmail.com>
2021-10-27 00:59:43 +0900

    Cell Information表示機能を追加

--- app/src/main/java/com/example/cellmonitor/MainActivity.kt
+++ app/src/main/java/com/example/cellmonitor/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.cellmonitor
 
+import android.Manifest
 import android.annotation.TargetApi
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Environment
 import android.provider.Settings
+import android.telephony.*
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.example.cellmonitor.databinding.ActivityMainBinding
+import java.lang.StringBuilder
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         android.Manifest.permission.READ_PHONE_STATE,
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
         android.Manifest.permission.READ_EXTERNAL_STORAGE
-
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +39,173 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             checkLogPermission()
         }
+
+        binding.showCell.setOnClickListener {
+            //Handler を用いてTimerの処理をUIスレッドで行うようにする
+            //画面に関する処理はUIスレッドで行う必要がある
+            val handler = Handler(Looper.getMainLooper())
+            timer(period = 1000,initialDelay = 1000){
+                handler.post {
+                    Log.d(TAG,"Handler Posted")
+                    cellInfoToTV()
+                }
+            }
+        }
+    }
+
+    //戻るボタンを押した際にダイアログを表示
+    override fun onBackPressed() {
+        val dialog = BackPressedDialogFragment()
+        dialog.show(supportFragmentManager,"id")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"onDestroy")
+        super.onDestroy()
     }
 
     //Permissionチェックのメソッド
     fun checkPermission(permissions: Array<String>?, request_code: Int) {
         // 許可されていないものだけダイアログが表示される
         ActivityCompat.requestPermissions(this, permissions!!, request_code)
+    }
+
+    private fun cellInfoToTV(){
+        getCellInformation { cellInfoList ->
+            val sb = StringBuilder()
+            val length = cellInfoList.size
+            sb.append("取得Cell：${length}個")
+            sb.append("\n")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                for (cellInfo in cellInfoList){
+                    when(cellInfo){
+                        is CellInfoLte ->{
+                            Log.d(TAG,"CellInfoLTE")
+                            val timeStamp = cellInfo.timeStamp
+                            val cellID = cellInfo.cellIdentity.ci
+                            val mccmnc = cellInfo.cellIdentity.mobileNetworkOperator
+                            val EARFCN = cellInfo.cellIdentity.earfcn
+                            val pci = cellInfo.cellIdentity.pci
+                            val tac = cellInfo.cellIdentity.tac
+                            val rsrp = cellInfo.cellSignalStrength.rsrp
+                            val rsrq = cellInfo.cellSignalStrength.rsrq
+                            val rssi = cellInfo.cellSignalStrength.rssi
+                            val rssnr = cellInfo.cellSignalStrength.rssnr
+                            val cqi = cellInfo.cellSignalStrength.cqi
+
+                            sb.append("TimeStamp:${timeStamp}ns\n")
+                            sb.append("CellType:LTE\n")
+                            sb.append("CellID:$cellID\n")
+                            sb.append("MCC+MNC:$mccmnc\n")
+                            sb.append("CellType:LTE\n")
+                            sb.append("EARFCN:$EARFCN\n")
+                            sb.append("PCI:$pci\n")
+                            sb.append("TAC:$tac\n")
+                            sb.append("RSRP:$rsrp\n")
+                            sb.append("RSSQ:$rsrq\n")
+                            sb.append("RSSI:$rssi\n")
+                            sb.append("RSSNR$rssnr\n")
+                            sb.append("CQI:$cqi\n")
+                            sb.append("------------------")
+                            sb.append("\n")
+                        }
+                        is CellInfoNr->{
+                            Log.d(TAG,"CellInfoNR")
+                            val timeStamp = cellInfo.timeStamp
+                            val cellID = (cellInfo.cellIdentity as CellIdentityNr).nci
+                            val mcc =(cellInfo.cellIdentity as CellIdentityNr).mccString
+                            val mnc =(cellInfo.cellIdentity as CellIdentityNr).mncString
+                            val NRARFCN =(cellInfo.cellIdentity as CellIdentityNr).nrarfcn
+                            val pci =(cellInfo.cellIdentity as CellIdentityNr).pci
+                            val tac =(cellInfo.cellIdentity as CellIdentityNr).tac
+                            val ssRsrp =(cellInfo.cellSignalStrength as CellSignalStrengthNr).ssRsrp
+                            val ssRsrq =(cellInfo.cellSignalStrength as CellSignalStrengthNr).ssRsrq
+                            val ssSinr =(cellInfo.cellSignalStrength as CellSignalStrengthNr).ssSinr
+                            val csiRsrp = (cellInfo.cellSignalStrength as CellSignalStrengthNr).csiRsrp
+                            val csiRsrq =(cellInfo.cellSignalStrength as CellSignalStrengthNr).csiRsrq
+                            val csiSinr =(cellInfo.cellSignalStrength as CellSignalStrengthNr).csiSinr
+
+                            sb.append("TimeStamp:${timeStamp}ns\n")
+                            sb.append("CellType:NR\n")
+                            sb.append("NCI:$cellID\n")
+                            sb.append("MCC+MNC:${mcc+mnc}\n")
+                            sb.append("NRARFCN:$NRARFCN\n")
+                            sb.append("PCI:$pci\n")
+                            sb.append("TAC:$tac\n")
+                            sb.append("(SS)RSRP:$ssRsrp\n")
+                            sb.append("(SS)RSRQ:$ssRsrq\n")
+                            sb.append("(SS)SINR:$ssSinr\n")
+                            sb.append("(CSI)RSRP:$csiRsrp\n")
+                            sb.append("(CSI)RSRQ:$csiRsrq\n")
+                            sb.append("(CSI)SINR:$csiSinr\n")
+                            sb.append("------------------")
+                            sb.append("\n")
+                        }
+                    }
+                }
+            }
+            else{
+                for (cellInfo in cellInfoList){
+                    when(cellInfo){
+                        is CellInfoLte ->{
+                            Log.d(TAG,"CellInfoLTE")
+                            val timeStamp = cellInfo.timeStamp
+                            val cellID = cellInfo.cellIdentity.ci
+                  //          val mccmnc = cellInfo.cellIdentity.mobileNetworkOperator
+                            val EARFCN = cellInfo.cellIdentity.earfcn
+                            val pci = cellInfo.cellIdentity.pci
+                            val tac = cellInfo.cellIdentity.tac
+                            val rsrp = cellInfo.cellSignalStrength.rsrp
+                            val rsrq = cellInfo.cellSignalStrength.rsrq
+                  //          val rssi = cellInfo.cellSignalStrength.rssi
+                            val rssnr = cellInfo.cellSignalStrength.rssnr
+                            val cqi = cellInfo.cellSignalStrength.cqi
+
+                            sb.append("TimeStamp:${timeStamp}ns\n")
+                            sb.append("CellType:LTE\n")
+                            sb.append("CellID:$cellID\n")
+                    //        sb.append("MCC+MNC:$mccmnc\n")
+                            sb.append("CellType:LTE\n")
+                            sb.append("EARFCN:$EARFCN\n")
+                            sb.append("PCI:$pci\n")
+                            sb.append("TAC:$tac\n")
+                            sb.append("RSRP:$rsrp\n")
+                            sb.append("RSSQ:$rsrq\n")
+                      //      sb.append("RSSI:$rssi\n")
+                            sb.append("RSSNR$rssnr\n")
+                            sb.append("CQI:$cqi\n")
+                            sb.append("------------------")
+                            sb.append("\n")
+                        }
+                    }
+                }
+            }
+            binding.cellinfo.text = sb.toString()
+        }
+    }
+
+    private fun getCellInformation(result:(List<CellInfo>)-> Unit) {
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q){
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+               Toast.makeText(this,"位置情報の権限がONではありません。",Toast.LENGTH_SHORT).show()
+                return
+            }
+            telephonyManager.requestCellInfoUpdate(mainExecutor, object : TelephonyManager.CellInfoCallback() {
+                override fun onCellInfo(cellInfoList: MutableList<CellInfo>) {
+                    result.invoke(cellInfoList)
+                }
+            })
+        }else{
+            result.invoke(telephonyManager.allCellInfo)
+        }
+
     }
 
     // requestPermissionsのコールバック
@@ -73,6 +236,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 
     @TargetApi(Build.VERSION_CODES.R)
     private fun checkLogPermission(){
